e68c4755fb561f88f3173a22058d5cb04972d1de
AditVijay <adit.vjy10@gmail.com>
2017-09-18 11:28:21 +0530

    Update App.java

--- Test/src/main/java/com/cg/ui/App.java
+++ Test/src/main/java/com/cg/ui/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-	MyTest m=new Mytest();
+	MyTest m=new MyTest();
 	m.getData();
         System.out.println( "Hello World!" );
     }
